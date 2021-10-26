737d941835fcfb7b082d14933fca713aa6a72909
Derek Paul <dereksamuelgr@gmail.com>
2021-10-26 18:01:51 -0500

    first change to commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI;
+
+app = FastAPI();
+
+@app.get("/")
+def home():
+  return {
+    "Derek": "Es guapo",
+  };
+
+
+@app.get("/agent/{agent_id}")
+def home():
+  return {
+    "Derek": "Es guapo ID Id",
+  };
