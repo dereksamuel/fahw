397a1affebdcf533409ecc0723b603a68b7090e9
Derek Paul <dereksamuelgr@gmail.com>
2021-10-27 17:48:05 -0500

    added all to models

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
+#Python
+from typing import Optional;
+
+#Pydantic
+from pydantic import BaseModel;
+
+#FastApi
 from fastapi import FastAPI;
+from fastapi import Body;
 
 app = FastAPI();
+
+#Models
+class Person(BaseModel): # herencia de clases el basemodel
+  first_name : str;
+  last_name : str;
+  age : int;
+  hair_color : Optional[str] = None;
+  is_married : Optional[bool] = False;
+  abilities : dict = None;
 
 @app.get("/")
 def home():
@@ -8,8 +25,9 @@ def home():
     "Derek": "Es guapo",
   };
 
-@app.get("/agent/{agent_id}")
-def agentId():
-  return {
-    "Derek": "Es guapo ID Id",
-  };
+# Request and Responde Body
+
+@app.post("/agent/create")
+def new_agent(person: Person = Body(...)): # ... es obligatorio en python
+  # if (len(person["first_name"]))
+  return person;
