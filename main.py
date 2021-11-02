9ac1fde04549d03f2d910f17028f62a41df82624
Derek Paul <dereksamuelgr@gmail.com>
2021-11-02 15:22:35 -0500

    UploadFile

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from pydantic.networks import EmailStr, HttpUrl;
 #FastApi
 from fastapi import FastAPI;
 from fastapi import status;
-from fastapi import Body, Query, Path, Form, Header, Cookie;
+from fastapi import Body, Query, Path, Form, Header, Cookie, UploadFile, File;
 
 app = FastAPI();
 
@@ -228,3 +228,16 @@ def contact(
   ads: Optional[str] = Cookie(default=None),
 ):
   return user_agent;
+
+# Files and Uploads Files post
+@app.post(
+  path="/post-image",
+)
+def post_image(
+  image: UploadFile = File(...) # Type of var y el otro el valor de var
+):
+  return {
+    "Filename": image.filename,
+    "Format": image.content_type,
+    "Size(kb)": round(len(image.file.read()) / 1024, ndigits=2),
+  };
